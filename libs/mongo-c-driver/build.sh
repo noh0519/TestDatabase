e8892ee0e0025a5da539c4fc6f91491d45197550
root <noh0519@hanmail.net>
2021-06-18 04:07:15 +0000

    mongo-c-driver

---
+++ libs/mongo-c-driver/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+TARGET=mongo-c-driver-1.17.6
+TARGET_TAR=${TARGET}.tar.gz
--- mongodbcpp/main.cpp
+++ mongodbcpp/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "mongoc.h"
 
 int main(int argc, char** argv) {
     printf("Hello!\n");
+
+    mongoc_database_t *database;
+    mongoc_client_t *client;
 
     return 0;
 }
